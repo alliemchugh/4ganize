45c34ebfe709e5b902b559875d96a38f5be732c1
adelynchen <30806609+adelynchen@users.noreply.github.com>
2018-07-25 15:38:26 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+button = document.getElementById("button")
+button1 = document.getElementById("button1")
+button.style.background='#ccccff ';
+button1.style.background='#ccccff ';
+button.style.borderColor='#ccccff ';
+button1.style.borderColor='#ccccff ';
