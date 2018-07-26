762fe551b17016064155fab1dae413d69161dcc6
adelynchen <30806609+adelynchen@users.noreply.github.com>
2018-07-26 15:47:55 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,85 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+button = document.getElementById("button")
+button1 = document.getElementById("button1")
+button.style.background='#ccccff ';
+button.style.borderColor='#ccccff ';
+button1.style.background='#ccccff ';
+button1.style.borderColor='#ccccff ';
+button2.style.background='#ccccff ';
+button2.style.borderColor='#ccccff ';
+
+var content = document.getElementById("javascipt")
+
+$(document).ready(function() {
+  $('.stBtn').click(function() {
+    var btn = $('.stBtn');
+    var minutes = parseInt($('#min').html());
+    var seconds = parseInt($('#sec').html());
+    if (minutes + seconds === 0) {
+      btn.html('Start');
+      btn.removeClass('red');
+      alert('Please set the timer');
+      return;
+    }
+    if (isNaN(minutes)) {
+      $('#min').html('00');
+      minutes = 0;
+    }
+    if (minutes > 59) {
+      alert("Can't be used for more than 59 mins");
+      $('#min').html('00');
+      minutes = 0;
+      return;
+    }
+    if (isNaN(seconds)) {
+      $('#sec').html('00');
+      seconds = 1;
+      return;
+    }
+    if (isNaN(minutes))
+
+    function countdownTimer(t) {
+      var secLeft = Math.floor(t % 60);
+      var minLeft = Math.floor((t / 60) % 60);
+      var dispSeconds = '0' + secLeft;
+      var dispMinutes = '0' + minLeft;
+      $('#min').html(dispMinutes.slice(-2));
+      $('#sec').html(dispSeconds.slice(-2));
+    }
+  function reStart() {
+      btn.html('Start');
+        btn.removeClass('red');
+      }
+    function updateTimer() {
+      t--;
+      countdownTimer(t);
+      if (t <= 0) {
+        clearInterval(interval);
+        $('#ado').get(0).play();
+        reStart();
+      }
+    }
+    if (btn.html() == "Stop") {
+      clearInterval(interval);
+      reStart();
+    } else {
+      window.interval = setInterval(updateTimer, 1000);
+      btn.html('Stop');
+      btn.addClass('red');
+    }
+  });
+
+  $('.refresh').click(function() {
+    clearInterval(interval);
+    $('#min').html('00');
+    $('#sec').html('00');
+    $('.stBtn').html('Start');
+    $('.stBtn').removeClass('red');
+  });
+});
